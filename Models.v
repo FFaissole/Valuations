f63e0d0ec91d6076cf3bd0373160c0da6a7e0666
FFaissole <florianfaissole@gmail.com>
2016-09-14 12:28:16 +0200

    some new proofs

--- Models.v
+++ Models.v
@@ -10,30 +10,69 @@ Require Import HoTT.HSet HoTT.Basics.Trunc HProp HSet
                TruncType UnivalenceAxiom Types.Sigma
                FunextVarieties. 
 
-Require Import LowerR Distr Dedekind_pos.
+Require Import Qaxioms LowerR Distr Dedekind_pos.
 
 Set Implicit Arguments.  
 
 Definition Phao (f : Sier -> Sier) := forall (u : Sier), 
     (f u) <-> join (meet (f SierTop) u) (f SierBot).
 
-Definition Dominance := forall (s : Sier) (p : Type),
-           (IsTop s -> p) -> (exists v, IsTop v = (IsTop s) /\ p). 
-
-Lemma Dom : Dominance. 
+Definition Dominance : forall (s : Sier) (p : Type), Sier.  
 Proof.
-intros s p H.
-generalize (partial_rec Unit Sier SierLe).
+intros s p.   
+revert s; apply (partial_rec Unit _ le).   
+simple refine (Build_Recursors _ _ _ _ _ _ _ _ _ _ _ _);simpl.
++ intros _; exact SierTop. 
++ exact bottom. 
++ intros f Hf. 
+  apply sup.
+  exists f. exact Hf. 
++ reflexivity.
++ apply bot_least.   
++ intros ss pp xx IH;apply (sup_le_l _ _ _ IH). 
++ intros ss pp xx IH; apply sup_le_r. apply IH.  
+Defined. 
+
+(*
+Lemma dominance_correct : forall (p : Type) (s : Sier),
+          (IsTop s -> p) -> IsTop (Dominance s p) <-> (IsTop s /\ p). 
+Admitted. *)
+
+Lemma Phao_mon : forall (f : Sier -> Sier), (forall x y, x <= y -> f x <= f y)
+                                           -> Phao f.  
+Proof.
+intros f Hf. 
+unfold Phao.   
+intros u.
+split; intros H.
+revert H.
+revert u. 
+apply (partial_ind0 _ (fun s  =>  _ -> _)).
++ intros [] Hx. 
+  specialize (Hf SierBot SierTop). 
+  assert (Hf' : SierBot <= SierTop). 
+  apply top_greatest. specialize (Hf Hf'). clear Hf'. 
+  assert (Hf2 : f SierBot <= eta Unit tt).   
+  apply top_greatest. 
+  assert (Hff : f SierBot <= (f SierTop ⊓ eta Unit tt)). 
+  apply imply_le.
+  intros Hu. 
+  apply top_le_meet.
+  split. 
+  - trivial.  
+  - apply SierLe_imply in Hf2; trivial.
+  - apply top_le_join. apply tr.
+    left.     
+    apply top_le_meet. 
+    split; try trivial. 
+    apply top_greatest. 
++ intros Hx. 
+  apply top_le_join.
+  apply tr. right.         
+  trivial.     
++ intros g H0 H1. 
+  admit. 
 Admitted.
-
-Definition Scott_cont (A B : Type) : (A -> B) -> Type.
-Admitted.
-
-Lemma Phao_scott : forall (f : Sier -> Sier), Scott_cont f -> Phao f.  
-Proof. 
-Admitted. 
-
-(* dominance : how to express it ? *)
 
 Record No := mkNo{
                  u :> nat -> bool;
@@ -48,8 +87,6 @@ Defined.
 Definition WSO := forall (O : No -> Sier), O (infNo)
     -> exists un : No, (O un) /\ (exists p, (un p) = false).  
 
-Search list. 
-
 Fixpoint nth (n:nat) (l:list bool) {struct l} : option bool :=
     match n, l with
       | O, cons x l' => Some x
@@ -58,13 +95,14 @@ Fixpoint nth (n:nat) (l:list bool) {struct l} : option bool :=
       | S m, cons x t => nth m t
     end.
 
-Definition bars_aux (l : list bool) (a : nat -> option bool) (n : nat) := a n = nth n l. 
+Definition bars_aux (l : list bool) (a : nat -> option bool)
+                   := exists (n:nat), a n = nth n l. 
 
-(*
 Inductive bars (a : (nat -> option bool)) : (list (list bool)) -> Prop :=
-          | bars_empty : bars a nil
-          | bars_coll : forall c l, exists n:nat, (bars_aux c a n)
-          | bars_coll2 : forall c l, bars a l end. 
-          
-Definition Fan := forall (u : nat -> bool), 
-)*)
+      | bars_empty : bars a nil
+      | bars_head : forall c, bars_aux c a -> bars a (cons c nil)
+      | bars_tail : forall c l, bars a l -> bars a (cons c l). 
+
+
+
+
