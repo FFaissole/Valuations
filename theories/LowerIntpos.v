a87e57912a9d221e460473409db265b88202ab70
FFaissole <florianfaissole@gmail.com>
2016-10-14 21:52:37 +0200

    new names in import

--- theories/LowerIntpos.v
+++ theories/LowerIntpos.v
@@ -9,7 +9,7 @@ Require Import HoTT.HSet HoTT.Basics.Trunc HProp HSet
                TruncType UnivalenceAxiom Types.Sigma
                FunextVarieties hit.quotient. 
 
-Require Import Qaxioms Duilq Distr2.
+Require Import Qaxioms RoundedClosed Valuations.
 
 Set Implicit Arguments.  
 
