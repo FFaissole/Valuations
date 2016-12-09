810d24a1f16ce5493817884d38d91f063911a1f9
Florian Faissole <florianfaissole@gmail.com>
2016-12-09 17:24:21 +0100

    integrals and val

---
+++ Theories/LowerIntegrals.v
@@ -0,0 +1,95 @@
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/HoTTClasses/theories".
+
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Spaces".
+
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Theories".
+
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Orders".
+
+
+Require Import HoTTClasses.interfaces.abstract_algebra
+               HoTTClasses.interfaces.orders
+               HoTTClasses.implementations.sierpinsky
+               HoTTClasses.implementations.dedekind
+               HoTTClasses.theory.rationals
+               HoTTClasses.theory.premetric. 
+Require Import HoTT.HSet HoTT.Basics.Trunc HProp HSet
+               Types.Universe UnivalenceImpliesFunext
+               TruncType UnivalenceAxiom Types.Sigma
+               FunextVarieties hit.quotient. 
+
+Require Import RoundedClosed
+               Functions
+               Valuations. 
+               
+Set Implicit Arguments.
+
+Section Integrals.
+
+
+
+Definition M (A : hSet) := mf A -> RlowPos. 
+
+Definition Mplus {A} : Plus (M A). 
+intros I J f.
+specialize (I f).
+specialize (J f).
+refine (RlP_plus I J).
+Defined. 
+
+Definition Mdef {A} (I : M A) :=
+              (I (fzero A)) = RlP_0.
+
+Definition Mprob {A} (I : M A) :=
+               (I (fone A)) <= RlP_1. 
+
+Definition Mstable_add {A} (I : M A) :=
+  forall f g: mf A, (I (fplus f g)) = ((I f)+(I g)).
+
+Definition Mpos {A} (I : M A) :=
+  forall (f : mf A), (forall x, RlP_0 <= f x) -> RlP_0 <= I f.
+
+Definition Mmon {A} (I : M A) := 
+   forall f g : mf A,  f <= g -> (I f) <= (I g).
+
+Definition Mcont {A} (I : M A) :=
+  forall f : mf A, I f = RllubPos (fun n => I (fun x => (f x))). 
+
+Global Instance MPos_semi_group {A} : SemiGroup (M A)
+                                          (Aop := @Mplus A). 
+Proof. 
+split. 
++ apply _.   
++ hnf. intros x y z.
+  unfold sg_op, plus_is_sg_op.
+  apply path_forall; intros q.
+  unfold Mplus. 
+  rewrite RlPPlus_assoc.
+  reflexivity.   
+Defined. 
+
+(** Integrals are definite, sigma-additive, monotonic and 
+associated to probability *)
+Record IntPos (A : hSet) : Type := 
+  {I : M A;
+   I_def : Mdef I;
+   I_add : Mstable_add I;
+   I_prob : Mprob I;
+   I_mon : Mmon I;
+   I_cont : Mcont I
+}.
+
+Hint Resolve I_def I_add I_prob I_mon I_cont. 
+
+Lemma Ieq_ext {A} (f g : mf A) (It : IntPos A) :
+         (forall x, (f x) = (g x)) -> (I It f) = (I It g). 
+Proof.
+intros HH.
+destruct It. simpl. 
+assert (Hfg : f = g).
+apply path_forall.
+intros x; trivial.
+rewrite Hfg; reflexivity.
+Qed.
+
+End Integrals. 
---
+++ Theories/Valuations.v
@@ -0,0 +1,80 @@
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/HoTTClasses/theories".
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Spaces".
+
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Theories".
+
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Orders".
+Require Import HoTTClasses.interfaces.abstract_algebra
+               HoTTClasses.interfaces.orders
+               HoTTClasses.implementations.sierpinsky
+               HoTTClasses.orders.orders. 
+Require Import HoTT.HSet HoTT.Basics.Trunc HProp HSet
+               Types.Universe UnivalenceImpliesFunext
+               TruncType UnivalenceAxiom hit.quotient
+               Basics.FunextVarieties FunextAxiom.
+
+Require Export RoundedClosed Opens Cpo.
+
+Section Val. 
+
+(*Context `{Funext} `{Univalence}.*)
+
+(** Space of valuations on A *) 
+Definition Mes (A : hSet) := OS A -> RlowPos.
+
+(* Definition of the properties *)
+Definition empty_op {A} (m : Mes A)  := (m ∅) =  RlP_0.
+
+Definition modular  {A} (m : Mes A) :=
+   forall (U V : OS A),  (RlP_plus (m U) (m V)) = 
+                           (RlP_plus (m (U ∪ V)) (m (U ⋂ V))).
+
+Definition seq_open_mes  {A} (m :Mes A) :
+         (nat -> OS A) -> (nat -> RlowPos).
+Proof. 
+intros L n. 
+specialize (L n).
+specialize (m L).
+refine m.
+Defined.
+
+Definition mon_opens  {A} (m : Mes A) :=
+   forall (U V : OS A), U ⊆ V -> (m U) <= (m V).
+
+(*Space of distributions*)
+Record D  (A : hSet) : Type :=
+  {mu :> @Mes A;
+   mu_modular : modular mu; 
+   mu_empty_op : empty_op mu;
+   mu_mon : mon_opens mu;
+   mu_prob : (mu Ω) <= (RlP_1)
+}.
+
+Hint Resolve mu_modular mu_prob mu_empty_op mu_mon.
+
+(** *** Properties of measures *)
+
+(* mu is monotonic *) 
+Lemma mu_monotonic : forall {A} (m : D A), mon_opens m.
+Proof.  auto. Qed.
+Hint Resolve mu_monotonic.
+
+(* eq stable *)
+
+Lemma mu_stable_eq : forall  {A} (m: D A) (U V : OS A),
+    U = V -> (mu A m U) = (mu A m V).
+Proof. 
+intros A m U V H2.
+rewrite H2. 
+split; auto.   
+Qed.
+
+Hint Resolve mu_stable_eq.
+
+(* mu m (fone A) <= 1%RlPos *)
+Lemma mu_one : forall  {A} (m: D A), (mu A m Ω) <=  RlP_1.
+Proof. auto. Qed. 
+
+Hint Resolve mu_one.
+
+End Val. 
