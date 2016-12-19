28b9bd22e4ca945a34dde11ac8e924ad2cf4c90d
Florian Faissole <florianfaissole@gmail.com>
2016-12-19 14:57:00 +0100

    progress in modularity

--- RIesz/OpenFun.v
+++ RIesz/OpenFun.v
@@ -1,4 +1,13 @@
 
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/HoTTClasses/theories".
+
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Spaces".
+
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Theories".
+
+Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Orders".
+
+
 Require Import HoTTClasses.interfaces.abstract_algebra
                HoTTClasses.interfaces.orders
                HoTTClasses.implementations.partiality
@@ -61,12 +70,14 @@ apply (partialLe_ind0 _ _).
   intros. apply RlowPos_pos.
 + intros f x H1 H2 n.
   transitivity (OpenFun_aux (sup Unit f)).
-  assert (Hr : OpenFun_aux (sup Unit f) = RllubPos (fun n => OpenFun_aux (f n))).
+  assert (Hr : OpenFun_aux (sup Unit f) = RllubPos
+                        (fun n => OpenFun_aux (f n))).
   auto. rewrite Hr.
   apply (Rllub_lub (fun n => OpenFun_aux (f n))); trivial.
   trivial.
 + intros f x H1 H2.
-  assert (Hr : OpenFun_aux (sup Unit f) = RllubPos (fun n => OpenFun_aux (f n))).
+  assert (Hr : OpenFun_aux (sup Unit f) = RllubPos
+                        (fun n => OpenFun_aux (f n))).
   auto. rewrite Hr.
   apply Rllub_le.
   intros n.
@@ -74,7 +85,8 @@ apply (partialLe_ind0 _ _).
 Qed.
       
 (* Map from Opens to characteristic function *)
-Definition OpenFun (A : hSet) : forall (U : A -> Sier), (A -> RlowPos). 
+Definition OpenFun (A : hSet) : forall (U : A -> Sier),
+                                       (A -> RlowPos). 
 Proof. 
 intros s z. 
 specialize (s z).
@@ -107,20 +119,102 @@ unfold OpenFun.
 apply OpenFun_aux_mon; trivial.
 apply (H1 s).
 Qed.
+(*
 
 Lemma OpenFun_meet_is_meet {A}: forall (U V : OS A) s r,
       (val (rl ((OpenFun _ U) s)) r
     /\ val (rl ((OpenFun _ V) s)) r) <->
        val (rl ((OpenFun_aux ((SierMeet (U s) (V s)))))) r. 
-Proof. 
+Proof.
+intros U V r s.  
+split.
++ intros (H1,H2).
+  unfold OpenFun in H1.
+  revert H1. 
+  apply SierLe_imply. unfold val. admit.   
++ intros Hm. 
+  
+Admitted. *)
+
+Definition OpenFun_meet {A} (U V : OS A) := OpenFun _ (OS_meet U V).
+
+Lemma OpenFun_siermeet_is_meet A : forall (a b : Sier) r s,
+      ((val (rl ((OpenFun A (fun x => a)) r)) s)
+   /\ (val (rl ((OpenFun A (fun x => b)) r)) s)) <-> 
+       (val (rl ((OpenFun A (fun x => SierMeet a b)) r)) s).  
+Proof.
+split; revert a b.  
+apply (partial_ind0 _ (fun a => forall b, _ -> _)).
+simpl. unfold semi_decide. destruct (decide (s < 1)).
+intros x b (H1,H2).
+assert (SierMeet (eta Unit x) b = b). 
+reflexivity. 
+rewrite X; trivial.
 Admitted. 
 
-Lemma OpenFun_join_is_join  {A}: forall (U V:OS A) s r, 
+Lemma OpenFun_meet_is_meet {A}: forall (U V : OS A) s r,
+      (val (rl ((OpenFun _ U) s)) r
+    /\ val (rl ((OpenFun _ V) s)) r) <->
+       val (rl ((OpenFun_meet U V) s)) r. 
+Proof.
+intros U V r s.  
+split.
++ intros (H1,H2).
+  unfold OpenFun_meet.
+  unfold OS_meet.  
+   
+  unfold val.  admit.
+  
++ intros HH. 
+  split.
+  - apply RC_mon with Qle (rl (OpenFun_aux ((OS_meet U V) r))) s.
+    intros x y; apply (antisymmetry le). 
+    intros x y; apply le_or_lt. 
+    reflexivity. 
+    apply OpenFun_aux_mon. 
+    apply SierMeet_is_meet.
+    apply HH. 
+  - apply RC_mon with Qle (rl (OpenFun_aux ((OS_meet U V) r))) s.
+    intros x y; apply (antisymmetry le). 
+    intros x y; apply le_or_lt. 
+    reflexivity. 
+    apply OpenFun_aux_mon. 
+    apply SierMeet_is_meet.
+    apply HH. 
+Admitted.
+
+
+Definition OpenFun_join {A} (U V : OS A) := OpenFun _ (OS_join U V). 
+
+Lemma OpenFun_join_is_join {A}: forall (U V : OS A) s r,
       (val (rl ((OpenFun _ U) s)) r
     \/ val (rl ((OpenFun _ V) s)) r) <->
-       val (rl ((OpenFun_aux ((SierJoin (U s) (V s)))))) r. 
+       val (rl ((OpenFun_join U V) s)) r. 
 Proof.
-Admitted.
+intros U V r s.  
+split.
++ intros HD. case HD; intros HDO. 
+  - unfold OpenFun_meet.
+    apply RC_mon with Qle (rl (OpenFun_aux (U r))) s.
+    intros x y; apply (antisymmetry le). 
+    intros x y; apply le_or_lt. 
+    reflexivity. 
+    apply OpenFun_aux_mon. 
+    apply SierJoin_is_join.
+    apply HDO.
+  - unfold OpenFun_meet.
+    apply RC_mon with Qle (rl (OpenFun_aux (V r))) s.
+    intros x y; apply (antisymmetry le). 
+    intros x y; apply le_or_lt. 
+    reflexivity. 
+    apply OpenFun_aux_mon. 
+    apply SierJoin_is_join.
+    apply HDO.  
++ intros HH.
+  unfold OpenFun_join in HH.
+  unfold OS_join in HH.
+  admit. 
+Admitted. 
 
   (* new definitions, new proof, to fix soon *)
 Lemma OpenFun_mod {A}: forall (U V : OS A), fplus (OpenFun _ U) (OpenFun _ V) =
@@ -144,13 +238,12 @@ apply (antisymmetry le).
   - exists r, s. 
     repeat split; try trivial.
     * unfold OS_meet.
-      assert (HrV : val (rl (OpenFun_aux (V z))) r). 
+      assert (HrV : val (rl (OpenFun A V z)) r). 
       apply RC_le with Qle s. 
       intros ss ss'; apply (antisymmetry le). 
       apply le_or_lt. apply Hs. apply l. 
-      (*apply OpenFun_meet_is_meet. split; trivial. *)
-      admit.
-     
+      apply OpenFun_meet_is_meet.            
+       split; trivial.
     * apply RC_mon with Qle (rl (OpenFun_aux (V z))) s. 
       intros ss ss'; apply (antisymmetry le). 
       apply le_or_lt. reflexivity. 
@@ -164,8 +257,8 @@ apply (antisymmetry le).
       apply RC_le with Qle r. 
       intros ss ss'; apply (antisymmetry le). 
       apply le_or_lt. apply Hr. apply l.  
-      (*apply OpenFun_meet_is_meet. 
-      split; trivial.   *) admit.         
+      apply OpenFun_meet_is_meet. 
+      split; trivial.   
     * apply RC_mon with Qle (rl (OpenFun_aux (U z))) r. 
       intros ss ss'; apply (antisymmetry le). 
       apply le_or_lt. reflexivity. 
@@ -175,8 +268,7 @@ apply (antisymmetry le).
     * rewrite Hq. 
       rewrite rings.plus_comm.  
       reflexivity. 
- -  apply Rlplus_eq_pr in Ho.
-    simpl in Ho. (* trivial *) admit. 
+  - apply pred_plus_pr; trivial.  
 + intros q Hs. simpl in *.
   apply rlow_pred_plus_pr in Hs.
   assert (Ho : merely
@@ -188,20 +280,20 @@ apply (antisymmetry le).
   intros Hs; apply tr. 
   destruct Hs as (r,(s,(Hr,(Hs,Hq)))).
   destruct (Qle_total r s).
-   - (*apply OpenFun_join_is_join in Hs.
+   - apply OpenFun_join_is_join in Hs.
      destruct Hs. 
      -- exists s, r.               
         repeat split; try trivial. 
         * apply OpenFun_meet_is_meet in Hr. 
           destruct Hr as (Hr1,Hr2). 
           trivial.  
-        * rewrite (plus_comm). 
+        * rewrite (rings.plus_comm). 
           trivial.
      -- exists r, s. 
         repeat split; try trivial. 
         apply OpenFun_meet_is_meet in Hr. 
         destruct Hr as (Hr1,Hr2). 
-        trivial.  *) admit. 
+        trivial.  
    - exists r, r. 
      repeat split; try trivial.
      * apply RC_mon with Qle (rl (OpenFun _ (OS_meet U V) z)) r. 
@@ -215,12 +307,25 @@ apply (antisymmetry le).
        apply le_or_lt. reflexivity.
        apply OpenFun_aux_mon. 
        apply SierMeet_is_meet.
-       apply Hr.  
-     * transitivity (r + s). 
-       trivial. 
-      
-       admit. 
-   - apply rlow_pred_plus_pr in Ho; trivial. 
-Admitted.
+       apply Hr.
+     * apply lt_le_trans with (r + s); trivial.  
+       apply semirings.plus_le_compat; try reflexivity; trivial. 
+   - apply pred_plus_pr.      
+     revert Ho; apply (Trunc_ind _). 
+     intros (r,(s,(Ho1,Ho2))); apply tr. 
+     exists r.
+     exists (q - r). 
+     split; try trivial.      
+     split. 
+     * apply RC_le with Qle s. 
+      -- intros x y; apply (antisymmetry le). 
+      -- intros x y; apply le_or_lt.       
+      -- apply (fst Ho2). 
+      -- apply rings.flip_le_minus_l.
+         rewrite rings.plus_comm.
+         apply lt_le. 
+         apply (snd Ho2). 
+     * apply rings.plus_conjugate_alt.
+Qed.
 
 End OpenFun. 
