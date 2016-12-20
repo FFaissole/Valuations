aae7c3b106ff5eea2819155437559c7a4cee8ac6
Florian Faissole <florianfaissole@gmail.com>
2016-12-20 13:54:08 +0100

    new goals proved --> riesz hom1 (mu_I)

--- RIesz/Riesz.v
+++ RIesz/Riesz.v
@@ -1,12 +1,4 @@
 
-Add Rec LoadPath "/Users/faissole/Desktop/HoTT/HoTTClasses/theories".
-
-Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Spaces".
-
-Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Theories".
-
-Add Rec LoadPath "/Users/faissole/Desktop/HoTT/Measures/CoqPL/Orders".
-
 
 Require Import HoTTClasses.interfaces.abstract_algebra
                HoTTClasses.interfaces.orders
