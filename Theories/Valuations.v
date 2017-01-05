6c3b9a210e6e382d572845a0e9aa8f812abab36c
Florian Faissole <florianfaissole@gmail.com>
2017-01-05 21:25:55 +0100

    remove useless lemmas mu_def mu_one

--- Theories/Valuations.v
+++ Theories/Valuations.v
@@ -1,4 +1,3 @@
-
 Require Import HoTTClasses.interfaces.abstract_algebra
                HoTTClasses.interfaces.orders
                HoTTClasses.implementations.sierpinsky
@@ -12,12 +11,14 @@ Require Export RoundedClosed Opens Cpo.
 
 Section Val. 
 
-(*Context `{Funext} `{Univalence}.*)
+(** * Valuations on A: a valuation is the equivalent of 
+measures but on open sets. A valuation is semi_continuous and 
+is valued in the positive lower reals *)
 
 (** Space of valuations on A *) 
 Definition Mes (A : hSet) := OS A -> RlowPos.
 
-(* Definition of the properties *)
+(** Definition of the suitable properties *)
 Definition empty_op {A} (m : Mes A)  := (m ∅) =  RlP_0.
 
 Definition modular  {A} (m : Mes A) :=
@@ -36,8 +37,13 @@ Defined.
 Definition mon_opens  {A} (m : Mes A) :=
    forall (U V : OS A), U ⊆ V -> (m U) <= (m V).
 
-(*Space of distributions*)
-Record Val  (A : hSet) : Type :=
+(** Space of distributions: 
+     - modularity: mu U + mu V = 
+          mu (U /\ V) + mu (U \/ V) 
+     - definite : mu ∅ = 0
+     - monotonicity 
+     - sub-probability : mu A <= 1 *)
+Record D  (A : hSet) : Type :=
   {mu :> @Mes A;
    mu_modular : modular mu; 
    mu_empty_op : empty_op mu;
@@ -47,29 +53,11 @@ Record Val  (A : hSet) : Type :=
 
 Hint Resolve mu_modular mu_prob mu_empty_op mu_mon.
 
-(** *** Properties of measures *)
+(** Deductible properties of valuations *)
 
-(* mu is monotonic *) 
-Lemma mu_monotonic : forall {A} (m : Val A), mon_opens m.
+(** mu is monotonic *) 
+Lemma mu_monotonic : forall {A} (m : D A), mon_opens m.
 Proof.  auto. Qed.
 Hint Resolve mu_monotonic.
-
-(* eq stable *)
-
-Lemma mu_stable_eq : forall  {A} (m: Val A) (U V : OS A),
-    U = V -> (mu A m U) = (mu A m V).
-Proof. 
-intros A m U V H2.
-rewrite H2. 
-split; auto.   
-Qed.
-
-Hint Resolve mu_stable_eq.
-
-(* mu m (fone A) <= 1%RlPos *)
-Lemma mu_one : forall  {A} (m: Val A), (mu A m Ω) <=  RlP_1.
-Proof. auto. Qed. 
-
-Hint Resolve mu_one.
 
 End Val. 
