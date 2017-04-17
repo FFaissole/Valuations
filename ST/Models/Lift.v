2339e71380ad2947ac9514b3a71f87ad0e716e0f
Florian Faissole <florianfaissole@gmail.com>
2017-04-17 14:32:49 +0200

    a bit more in the proof

--- ST/Models/Lift.v
+++ ST/Models/Lift.v
@@ -19,37 +19,78 @@ Require Import HoTT.HSet HoTT.Basics.Trunc HProp HSet
 
 Section lift.
 
-Definition extend : partial A -> Sier.
+
+Definition umap : A -> Unit := fun x => tt.
+
+Definition extend : partial A -> Sier := partial_map umap.
+
+Lemma equiv_istop_umap : forall y, IsTop (extend y) -> 
+                       hexists (fun w => eta A w = y).
 Proof.
-apply (partial_rec A _ le).
-simple refine (Build_Recursors _ _ _ _ _ _ _ _ _ _ _ _);simpl.
-+ intros _. exact SierTop. 
-+ exact SierBot. 
-+ intros f Hn.
-  generalize (Build_IncreasingSequence f Hn).
-  intros ff.
-  refine (sup Unit ff). 
-+ reflexivity.
-+ intros x.
-  apply imply_le; intros b.
-  apply not_bot in b.
-  case b. 
-+ intros s Hp x Hi n.
-  simpl in Hi.
-  transitivity (sup Unit (Build_IncreasingSequence s Hp)).
-  apply imply_le; intros H.
-  apply top_le_sup.
-  apply tr; exists n.
-  apply H. trivial.
-+ intros s Hi x Hn.
-  simpl; apply imply_le.
-  intros H.
-  apply top_le_sup in H.
-  revert H.
-  apply (Trunc_ind _); intros (m,Hm).
-  revert Hm; apply SierLe_imply.
-  apply Hn. 
+apply (partial_ind0 _ (fun a => _ -> _)).
++ intros s Hs.
+  apply tr; exists s.
+  reflexivity.
++ intros Hk.
+  apply not_bot in Hk.
+  case Hk.
++ intros f Hf Hn.
+  assert (Hnn : hexists (fun n => extend (f n))).
+  apply top_le_sup in Hn.
+  trivial.
+  revert Hnn; apply (Trunc_ind _); 
+  intros (m,Hm).
+  apply Hf in Hm.
+  revert Hm; apply (Trunc_ind _).
+  intros (w,Hw).
+  apply tr.
+  exists w.
+  SearchAbout eta.
+  symmetry.
+  apply eta_is_greatest.
+  apply _.
+  rewrite Hw.
+  apply sup_is_ub.
+Qed.
+ 
+Definition map_lift : partial A -> {s : Sier | IsTop s ->  A}.
+Proof.  
+intros y.
+exists (extend y).
+intros H.
+generalize (equiv_istop_umap y H).
+intros Hg.
+unfold extend in H.
+assert (exists w:A, eta A w = y).
+generalize (unique_choice (fun n:Unit => 
+     (fun w:A => eta A w = y))).
+intros HH. 
+unfold hexists.
+assert (H1 :(Unit → ∀ y0 : A, IsHProp (eta A y0 = y))).
+intros X z.
+apply _.
+assert (H2 : (Unit → hunique (λ w : A, eta A w = y))).
+intros z.
+split.
+apply Hg.
+unfold atmost1P.
+intros a b Ha Hb.
+rewrite <- Hb in Ha.
+apply eta_le_eta.
+apply _.
+rewrite Ha; reflexivity.
+specialize (HH H1 H2).
+destruct HH as (f,Hff).
+exists (f tt).
+apply Hff.
+destruct X as (w,HX).
+refine w.
 Defined.
+
+Lemma map_lift_proj1 : forall y, (map_lift y).1 = extend y.
+Proof.
+intros y; simpl; reflexivity.
+Qed.
 
 Lemma equiv_istop_umap : forall y, IsTop (extend y) -> 
                        hexists (fun w => eta A w = y).
@@ -112,16 +153,6 @@ apply Hff.
 destruct X as (w,HX).
 refine w.
 Defined. 
-
-
-
-
-(*
-Record IsEquiv (A B : Type) (f : A → B) : Type := BuildIsEquiv
-  { equiv_inv : B → A;
-    eisretr : Sect equiv_inv f;
-    eissect : Sect f equiv_inv;
-    eisadj : ∀ x : A, eisretr (f x) = ap f (eissect x) }*)
 
 
 Definition map_lift_inv_aux (s : Sier) : 
